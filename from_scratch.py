469f737e5382b72cf0be369587a1b08b995bcaba
Saxon Knight <knight7@hawaii.edu>
2018-07-27 17:26:37 -1000

    Update from_scratch.py (technically-complete pipeline)

--- from_scratch.py
+++ from_scratch.py
@@ -13,8 +13,11 @@ from tensorflow import keras
 
 # settings
 max_vocab_size = 5000
-num_embedding_dims = 16
-num_hidden_nodes = 16
+num_embedding_dims = 32
+num_hidden_nodes = 32
+sequence_max_len = 100
+batch_size = 300
+epochs = 10
 
 # list of files of lines of tab-separated values
 text_dataset_filenames = [
@@ -32,6 +35,7 @@ for filename in text_dataset_filenames:
             data.append(line[:-3])
             labels.append(int(line[-2]))
 
+# later make method for new input cleaning
 # clean up data
 for i in range(len(data)):
     data[i] = sub('[^a-z0-9 ]+', '', data[i].lower())
@@ -67,8 +71,25 @@ model.add(keras.layers.Embedding(max_vocab_size, num_embedding_dims))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(num_hidden_nodes, activation=tf.nn.relu))
 model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
+model.summary()
+
+# configure for training
+model.compile(tf.train.AdamOptimizer(), 'binary_crossentropy', ['accuracy'])
+
+# later split data/labels into training, validation, and testing
+# produce training_data and training_labels
+training_data = []
+for datum in data:
+    training_data.append(encode_text_to_IDs(datum))
+training_data = keras.preprocessing.sequence.pad_sequences(training_data, sequence_max_len, padding='post', value=-1)
+training_labels = labels
 
 # train model
+model.fit(training_data, training_labels, batch_size, epochs)
 
-
-# test model
+## produce testing_data
+#testing_data = training_data
+#testing_labels = training_labels
+#
+## test model
+#test_results = model.evaluate(testing_data, testing_labels, batch_size)
